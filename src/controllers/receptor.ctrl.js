0a55cb08f924bf11d5531f0e556fb73272daf232
Kevin Acosta <acostakevin039@gmail.com>
2023-10-22 23:48:01 -0300

    tomar fecha hora

--- src/controllers/receptor.ctrl.js
+++ src/controllers/receptor.ctrl.js
@@ -5,6 +5,13 @@ const _receptorController = async (req, res) => {
         const datos = req.body
 
         console.log(datos)
+        const fechaHoraActual = new Date()
+        // Establecer la zona horaria a la de Paraguay (GMT-4)
+        fechaHoraActual.setUTCHours(fechaHoraActual.getUTCHours() - 3)
+        // Obtener el timestamp en milisegundos
+        const timestamp = fechaHoraActual.getTime()
+        console.log(fechaHoraActual)
+        console.log(timestamp)
         res.status(200).send({ error: "SIN_ERROR", desc: "Datos Recibidos" })
     } catch (error) {
         handleError(res, "receptorC", error)
